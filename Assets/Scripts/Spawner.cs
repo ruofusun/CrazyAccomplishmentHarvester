6e65aa5f3e256d7d414217cc18c1b78e2440b699
HoiSen <hoisenwong@outlook.com>
2023-01-08 18:12:53 -0500

    Scene3.0

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] ima;
+    float spawnTime = 0.0f;
+
+    GameObject player;
+    public bool enableSpawn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPos = player.transform.position;
+
+        spawnTime = spawnTime - Time.deltaTime;
+        if (spawnTime <= 0)
+        {
+            //var rockPos = Random.Range(4, 3);
+
+            Vector2 pos = new Vector2(4, 3);
+            //Quaternion.LookRotation()
+            int which = Random.Range(0, ima.Length);
+            GameObject obj=   Instantiate(ima[which], pos, Quaternion.identity);
+            obj.GetComponent<Rigidbody2D>().velocity = (playerPos - transform.position).normalized * 3;
+
+
+            spawnTime = Random.Range(1.0f, 2.0f);
+        }
+    }
+
+    public void EnableSpawn()
+    {
+        //enableSpawner = true;
+    }
+
+
+}
