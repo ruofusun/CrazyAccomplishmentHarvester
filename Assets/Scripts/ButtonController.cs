4ab385e1f4d4a2046ae6eb972beb002bbd638b09
ruofusun <42979525+ruofusun@users.noreply.github.com>
2023-01-08 15:41:23 -0500

    turtle scene prototype

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Fungus;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class ButtonController : MonoBehaviour
@@ -9,16 +11,38 @@ public class ButtonController : MonoBehaviour
     private Animator _animator;
 
     private bool triggered = false;
+
+    private bool canChasePlayer = false;
+    private Rigidbody2D rb2d;
+
+    private CharacterController2D _characterController2D;
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _characterController2D = FindObjectOfType<CharacterController2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
+    private Tween currentTween = null;
     // Update is called once per frame
     void Update()
     {
-        
+        if (canChasePlayer)
+        {
+            float dis = _characterController2D.transform.position.x - transform.position.x;
+            //|| (currentTween!=null&&currentTween.IsPlaying() && Mathf.Abs(dis)>3f)
+            if (currentTween == null|| (currentTween!=null&&!currentTween.IsPlaying()))
+            {
+               
+                if (Mathf.Abs(dis) > 1.5f)
+                {
+                    currentTween =
+                        rb2d.DOMove(new Vector2(_characterController2D.transform.position.x, rb2d.position.y), Mathf.Min(Mathf.Abs(dis)/3, 1.5f));
+                }
+                
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -36,5 +60,16 @@ public class ButtonController : MonoBehaviour
                 }
             }
         }
+    }
+
+    [Button]
+    public void TweenTest()
+    {
+        rb2d.DOMove(new Vector2(_characterController2D.transform.position.x, rb2d.position.y), 1f);
+    }
+
+    public void StartChasePlayer()
+    {
+        canChasePlayer = true;
     }
 }
