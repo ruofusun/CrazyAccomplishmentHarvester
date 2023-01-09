28d0e2b46991326e61b83df111ed2f01ef047ef0
Yining <yl10231@nyu.edu>
2023-01-08 20:22:50 -0500

    update

--- Assets/Scripts/GrandpaControllor.cs
+++ Assets/Scripts/GrandpaControllor.cs
@@ -13,7 +13,7 @@ public class GrandpaControllor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animGrandpa.GetComponent<Animator>();
+        GetComponent<Animator>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Level2ButtonControllor.cs
+++ Assets/Scripts/Level2ButtonControllor.cs
@@ -1,32 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Fungus;
 using UnityEngine;
 
 public class Level2ButtonControllor : MonoBehaviour
 {
     //public Animator camAnimator;
     public GameObject ouch;
-    public GameObject grandpa;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //camAnimator.GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        /*if (collision.tag == "Player")
         {
             Destroy(ouch, 0.5f);
-            Fungus.Flowchart.BroadcastFungusMessage("cleared");
-        }
+        }*/
     }
 }
